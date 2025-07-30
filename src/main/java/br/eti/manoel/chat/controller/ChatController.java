d5de3e9ded81c36523f023e8ba8b55a2a6eff841
Manoel Lima <manoelsslima@yahoo.com.br>
2025-07-30 02:41:49 -0400

    feat: add temporary cors grant

--- src/main/java/br/eti/manoel/chat/controller/ChatController.java
+++ src/main/java/br/eti/manoel/chat/controller/ChatController.java
@@ -3,13 +3,11 @@ package br.eti.manoel.chat.controller;
 import br.eti.manoel.chat.service.OpenAIService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/chat")
 public class ChatController {
